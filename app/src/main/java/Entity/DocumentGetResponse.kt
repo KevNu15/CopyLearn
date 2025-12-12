0930cf731ad8d08af960fda0f7283a48198dfe4f
Kevin Nunez <kenunezpa@est.utn.ac.cr>
2025-12-11 18:18:05 -0600

    Final

--- app/src/main/java/Entity/DocumentGetResponse.kt
+++ app/src/main/java/Entity/DocumentGetResponse.kt
@@ -3,8 +3,6 @@ package Entity
 /**
  * Respuesta de la API para endpoints GET
  * Devuelve una lista de documentos
- *
- * Siguiendo el patrón de PersonGetResponse en Census
  */
 data class DocumentGetResponse(
     val data: List<DTODocument>,
--- app/src/main/java/Entity/DocumentResponse.kt
+++ app/src/main/java/Entity/DocumentResponse.kt
@@ -3,8 +3,6 @@ package Entity
 /**
  * Respuesta de la API para endpoints POST, PUT y DELETE
  * Devuelve un solo documento (o null)
- *
- * Siguiendo el patrón de PersonResponse en Census
  */
 data class DocumentResponse(
     val data: DTODocument?,
